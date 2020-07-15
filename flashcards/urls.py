0e935da25aedca21620ac6a22ff215ad513c9da2
3xistential Crisis <55129160+3xistentialcrisis@users.noreply.github.com>
2020-07-15 23:24:30 +0300

    Add logout url pattern

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.contrib.auth import views
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,6 +8,8 @@ app_name = 'flashcards'
 
 urlpatterns = [
     url('^$', views.index, name='index'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 
 ]
 
